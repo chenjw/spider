5f48dfa57bd4c28dfd68f16860e2194c74e19216
chenjw <cjw1983@gmail.com>
2013-03-31 23:38:53 +0800

    update

---
+++ dt/app/src/main/java/com/chenjw/spider/dt/web/app/module/screen/Add.java
@@ -0,0 +1,14 @@
+package com.chenjw.spider.dt.web.app.module.screen;
+
+import com.alibaba.citrus.turbine.Context;
+import com.alibaba.citrus.turbine.Navigator;
+import com.chenjw.spider.dt.constants.EnvConstants;
+
+
+public class Add {
+
+	public void execute(Context context,
+			Navigator navigator) {
+		context.put("isProductMode", EnvConstants.isProductMode());
+	}
+}
